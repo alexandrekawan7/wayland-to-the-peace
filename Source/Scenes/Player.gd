f58b750cfe09a8074fc2925e4061a03bbe39384f
alexandrekawan7 <alexandrekawan2003@gmail.com>
2023-10-31 19:17:05 -0300

    movement proto

--- Source/Scenes/Player.gd
+++ Source/Scenes/Player.gd
@@ -1,37 +1,87 @@
 extends CharacterBody2D
 
 
-const SPEED = 1
+const SPEED = 3
 
-const DIRECTION_UP = -1
-const DIRECTION_LEFT = -1
-const DIRECTION_DOWN = 1
-const DIRECTION_RIGHT = 1
+const DIRECTION_UP = 1
+const DIRECTION_LEFT = 2
+const DIRECTION_DOWN = 3
+const DIRECTION_RIGHT = 4
+
+@onready var current_direction = DIRECTION_DOWN
+
+func flip():
+	$AnimatedSprite2D.flip_h = !$AnimatedSprite2D.flip_h
+
+func stay_idle() -> void:
+	match current_direction:
+			DIRECTION_UP:
+				if $AnimatedSprite2D.flip_h:
+					flip()
+				if $AnimatedSprite2D.animation != "idle_backward":
+					$AnimatedSprite2D.play("idle_backward")
+			DIRECTION_LEFT:
+				if not $AnimatedSprite2D.flip_h:
+					flip()
+				if $AnimatedSprite2D.animation != "idle_lateral":
+					$AnimatedSprite2D.play("idle_lateral")
+			DIRECTION_DOWN:
+				if $AnimatedSprite2D.flip_h:
+					flip()
+				if $AnimatedSprite2D.animation != "idle_frontal":
+					$AnimatedSprite2D.play("idle_frontal")
+			DIRECTION_RIGHT:
+				if $AnimatedSprite2D.animation != "idle_lateral":
+					$AnimatedSprite2D.play("idle_lateral")
+				
+				if $AnimatedSprite2D.flip_h:
+					flip()
+			
 
 func _ready():
-	$AnimatedSprite2D.play("idle")
+	stay_idle()
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("move_up"):
-		if $AnimatedSprite2D.animation == "idle":
-			$AnimatedSprite2D.stop()
+		if $AnimatedSprite2D.animation != "move_backward":
+			$AnimatedSprite2D.play("move_backward")
+		
+		if $AnimatedSprite2D.flip_h:
+			flip()
 			
+		current_direction = DIRECTION_UP
+		
 		position.y -= SPEED
 	elif Input.is_action_pressed("move_left"):
-		if $AnimatedSprite2D.animation == "idle":
-			$AnimatedSprite2D.stop()
+		if $AnimatedSprite2D.animation != "move_lateral":
+			$AnimatedSprite2D.play("move_lateral")
+		
+		if not $AnimatedSprite2D.flip_h:
+			flip()
 			
+		current_direction = DIRECTION_LEFT
+		
 		position.x -= SPEED
 	elif Input.is_action_pressed("move_down"):
-		if $AnimatedSprite2D.animation == "idle":
-			$AnimatedSprite2D.stop()
+		if $AnimatedSprite2D.animation != "move_frontal":
+			$AnimatedSprite2D.play("move_frontal")
+		
+		if $AnimatedSprite2D.flip_h:
+			flip()
 			
+		current_direction = DIRECTION_DOWN
+		
 		position.y += SPEED
 	elif Input.is_action_pressed("move_right"):
-		if $AnimatedSprite2D.animation != "move_right":
-			$AnimatedSprite2D.play("move_right")
+		if $AnimatedSprite2D.animation != "move_lateral":
+			$AnimatedSprite2D.play("move_lateral")
+		
+		if $AnimatedSprite2D.flip_h:
+			flip()
+			
+		current_direction = DIRECTION_RIGHT
 		
 		position.x += SPEED
 	else:
-		$AnimatedSprite2D.play("idle")
+		stay_idle()
 
